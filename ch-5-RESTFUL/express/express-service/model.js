aee226877cbd7aa6c0b7d36efe605fb842212a5b
freedom <sm_shaheer@msn.com>
2023-08-05 23:19:38 +0500

    second commit

---
+++ ch-5-RESTFUL/express/express-service/model.js
@@ -0,0 +1,21 @@
+'use stict'
+
+module.exports = {bicycle:bicycleModel()}
+
+function bicycleModel () {
+    const db = {
+    1: { brand: 'Veloretti', color: 'green' },
+    2: { brand: 'Batavus', color: 'yellow' }
+    }
+    return{
+        read
+    }
+    function read(id,cb){
+        if(!(db.hasOwnProperty(id))){
+            const err = Error('not found');
+            setImmediate(()=>cb(err))
+            return
+        }
+        setImmediate(()=>cb(null,db[id]))
+    }
+}
